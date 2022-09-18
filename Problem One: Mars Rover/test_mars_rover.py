e5d2024d2ccdb40f9eddb73750e53625d7f35c20
MarkMburu <marknjoroge@marknjorogetrayio.local>
2022-09-18 21:31:00 +0300

    Added unit test

--- Problem One: Mars Rover/test_mars_rover.py
+++ Problem One: Mars Rover/test_mars_rover.py
@@ -1,38 +1,24 @@
-import unittest
+import pytest
 from mars_rover.plateau import Plateau
 from mars_rover.position import Position
-from mars_rover.rover import Rover 
+from mars_rover.rover import Rover
 
+def test_position():
+    position = Position(0, 0)
+    assert position.x == 0
+    assert position.y == 0
+    position = Position(1, 2)
+    assert position.x == 1
+    assert position.y == 2
 
-class TestPosition(unittest.TestCase):
-    def testConstructor(self):
-        position = Position()
-        self.assertEqual(position.x, 0)
-        self.assertEqual(position.y, 0)
+def test_plateau():
+    plateau = Plateau(7, 10)
+    assert plateau.width == 7
+    assert plateau.height == 10
 
-        position = Position(1, 2)
-        self.assertEqual(position.x, 1)
-        self.assertEqual(position.y, 2)
-
-
-class TestPlateau(unittest.TestCase):
-    def testConstructor(self):
-        plateau = Plateau(7, 10)
-
-        self.assertEqual(plateau.width, 7)
-        self.assertEqual(plateau.height, 10)
- 
-
-class TestRover(unittest.TestCase):
-    def testConstructor(self):
-        plateau = Plateau(7, 7)
-        position = Position(0, 0)
-
-        rover = Rover(plateau, position, Rover.PATH['W'])
-
-        self.assertEqual(Position(0, 0), rover.position)
-        self.assertEqual(plateau, rover.plateau)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_rover():
+    plateau = Plateau(7, 7)
+    position = Position(0, 0)
+    rover = Rover(plateau, position, Rover.PATH['W'])
+    assert Position(0, 0) == rover.pos
+    assert plateau == rover.plateau
